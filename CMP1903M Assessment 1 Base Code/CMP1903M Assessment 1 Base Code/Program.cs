0d1b82a9fa8c7604b1256e3dccdadbc614892cfd
KinziBoi <94991890+KinziBoi@users.noreply.github.com>
2022-03-30 18:16:31 +0100

    Add files via upload

---
+++ CMP1903M Assessment 1 Base Code/CMP1903M Assessment 1 Base Code/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_Assessment_1_Base_Code
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Local list of integers to hold the first five measurements of the text
+            List<int> parameters = new List<int>();
+
+            //Create Input object
+            //Get either manually entered text, or text from a file
+            Input handleInput = new Input();
+            handleInput.userInput();
+            
+
+            //Create an Analyse object
+            //Pass the text input to the 'analyseText' method
+            Analyse analysis = new Analyse();
+            analysis.analyseText(handleInput.text);
+            
+
+            //Generate a report
+            Report output = new Report();
+            output.outputReport(analysis.values);
+            
+
+            //Need to still add frequency of letters
+
+           Console.ReadKey();
+        }
+        
+        
+    
+    }
+}
---
+++ CMP1903M Assessment 1 Base Code/CMP1903M Assessment 1 Base Code/Report.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_Assessment_1_Base_Code
+{
+    class Report
+    {
+        //Handles the reporting of the analysis
+        //Maybe have different methods for different formats of output?
+        //eg.   public void outputConsole(List<int>)
+        public void outputReport(List<int> vals)
+        {
+            Console.WriteLine("\nThe data Contained " + vals[0] + " Upper Case Letters \n");
+            Console.WriteLine("The data Contained " + vals[1] + " Lower Case Letters \n");
+            Console.WriteLine("The data contained " + vals[2] + " Sentences \n");
+            Console.WriteLine("The data Contained " + vals[3] + " Vowels \n");
+            Console.WriteLine("The data Contained " + vals[4] + " Consonants \n");
+        }
+
+    }
+}
